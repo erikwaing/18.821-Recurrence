79516f428f2d6bfc46cb7e5ed879608839aa848c
pkleinhe <pkleinhe@gmail.com>
2014-11-18 10:08:04 -0500

    Adding a simulation
    This code has two main methods: buildseq and periodic. buildseq takes in four inputs, the first two terms of the sequence, the function f(y) and the number of iterations, it outputs the recurrent sequence up to n terms as a list. periodic takes in one input a list and outputs the period of the list, if the list is not periodic it returns false. There are also some basic functions f(y).

---
+++ functiontester.py
@@ -0,0 +1,40 @@
+def buildseq(az, ao, function, n):
+    az = az +0.0
+    ao=ao+0.0
+    output=[ao,az]
+    for i in range(n):
+        add = function(ao)/az
+        output.append(add)
+        az=ao
+        ao=add
+    return output
+        
+def periodic(inputlist):
+    a = inputlist[0]
+    b = inputlist[1]
+    l = len(inputlist)
+    i=2
+    while i<l-2:
+        if inputlist[i]==a and inputlist[i+1]==b:
+            return i
+        else:
+            i = i+1
+    return False 
+        
+        
+
+def plusone(x):
+    return x+1
+def squared(x):
+    return x*x
+def plustwo(x):
+    return x+2
+def minusone(x):
+    return x-1
+def plusz(x):
+    return x+.5
+def line(x):
+    return 3*x+1
+b= buildseq(1,1,plustwo,400)
+#print b
+print periodic(b)
